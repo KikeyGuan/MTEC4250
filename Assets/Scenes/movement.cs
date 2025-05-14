151152cae024759c31df4e8e0b74324e7324da6a
KikeyGuan <113068463+KikeyGuan@users.noreply.github.com>
2025-05-14 13:44:11 -0400

    B A L L

--- Assets/Scenes/movement.cs
+++ Assets/Scenes/movement.cs
@@ -7,10 +7,17 @@ public class movement : MonoBehaviour
     public Rigidbody2D player;
     public float speed, torque, turn;
     private Vector2 move;
+    public Transform attachToPlayer;
+    private Vector2 insideCricle;
+    private Vector3 randomCricle;
+    public GameObject gameObj;
+    public float gatherArea; //checking
+    private int collectedItems;
     // Start is called before the first frame update
     void Start()
     {
         //turn = Input.GetAxisRaw("Horizontal");
+        randomCricle = Random.insideUnitCircle * gatherArea;
     }
 
     // Update is called once per frame
@@ -29,6 +36,25 @@ public class movement : MonoBehaviour
         }
         player.velocity = transform.up * move.y * speed;
         //player.AddTorque(transform.up * torque * turn);
+
+        insideCricle = gameObj.transform.position + randomCricle;
+        //expand cricle area
+        if (collectedItems == 2) {
+            //needs a STOP
+            //gatherArea += 1;
+            
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "item")
+        {
+            collectedItems += 1;
+            collision.gameObject.transform.SetParent(attachToPlayer);
+            collision.gameObject.transform.position = insideCricle;
+        }
 
     }
 }
