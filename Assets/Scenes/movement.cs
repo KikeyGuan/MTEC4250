70ccf63da5c2a8b8b17a844a0ce830fc4d60e69f
KikeyGuan <Kikey.Guan@mail.citytech.cuny.edu>
2025-05-20 01:19:15 -0400

    object collection

--- Assets/Scenes/movement.cs
+++ Assets/Scenes/movement.cs
@@ -1,59 +1,82 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class movement : MonoBehaviour
 {
     public Rigidbody2D player;
     public float speed, torque, turn;
     private Vector2 move;
-    public Transform attachToPlayer;
+    public Transform attachToPlayer; //the cake or the move object
     private Vector2 insideCricle;
     private Vector3 randomCricle;
-    public GameObject gameObj;
+    public GameObject cake; 
+    public CinemachineVirtualCamera camera;
     public float gatherArea; //checking
-    private int collectedItems;
     // Start is called before the first frame update
+    public int egg, milk, flour;
     void Start()
     {
         //turn = Input.GetAxisRaw("Horizontal");
-        randomCricle = Random.insideUnitCircle * gatherArea;
+        cake.transform.localScale = new Vector3(1f,1f,1f);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        randomCricle = Random.insideUnitCircle * gatherArea;
+
         move.y = Input.GetAxisRaw("Vertical");
         //Debug.Log (Input.GetAxisRaw("Horizontal"));
         
 
         if (Input.GetAxisRaw("Horizontal") == 1) {
-            transform.Rotate(0, 0, -1);
+            transform.Rotate(0, 0, -0.20f);
         }
         if (Input.GetAxisRaw("Horizontal") == -1)
         {
-            transform.Rotate(0, 0, 1);
+            transform.Rotate(0, 0, 0.20f);
         }
         player.velocity = transform.up * move.y * speed;
+        //move item to random spot in cake
+        insideCricle = attachToPlayer.transform.position + randomCricle;
         //player.AddTorque(transform.up * torque * turn);
 
-        insideCricle = gameObj.transform.position + randomCricle;
-        //expand cricle area
-        if (collectedItems == 2) {
-            //needs a STOP
-            //gatherArea += 1;
-            
+        if (egg >= 1 && flour >= 1 && milk >= 1){
+            gatherArea +=0.5f;
+            cake.transform.localScale = new Vector3(cake.transform.localScale.x+.2f,cake.transform.localScale.y+.2f,cake.transform.localScale.z+.2f);
+            if (camera.m_Lens.OrthographicSize != 15){
+                camera.m_Lens.OrthographicSize +=0.2f;
+            }
+            egg -= 1;
+            flour -= 1;
+            milk -= 1;
         }
 
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "item")
+        if (collision.gameObject.tag == "fruit" || 
+            collision.gameObject.tag == "sweet" || 
+            collision.gameObject.tag == "egg" ||
+            collision.gameObject.tag == "flour" ||
+            collision.gameObject.tag == "milk")
         {
-            collectedItems += 1;
             collision.gameObject.transform.SetParent(attachToPlayer);
             collision.gameObject.transform.position = insideCricle;
+            Destroy(collision.gameObject.GetComponent<CircleCollider2D>());
+            if(collision.gameObject.tag == "egg"){
+                egg +=1;
+            }
+            if(collision.gameObject.tag == "flour"){
+                flour +=1;
+            }
+            if(collision.gameObject.tag == "milk"){
+                milk +=1;
+            }
         }
 
     }
--- Assets/Scenes/spwan.cs
+++ Assets/Scenes/spwan.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class spwan : MonoBehaviour
 {
     private float xMin, xMax, yMin, yMax;
-    public GameObject fish1;
-    public GameObject fish2;
+    public int foodlimit = 100, foodSpawn;
+    public GameObject[] food;
+    public GameObject[] upgrade;
     //get object position, add 5 to get area, spwan object in said area.
     // Start is called before the first frame update
     void Start()
     {
+        /*
         xMin = transform.position.x - 5;
         xMax = transform.position.x + 5;
         yMin = transform.position.y - 5;
         yMax = transform.position.y + 5;
+        */
+
 
         
     }
@@ -22,6 +26,12 @@ public class spwan : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (foodSpawn != foodlimit) //would add respwan but too many items
+        {
+            Vector3 randomSpwanPos = new Vector3(Random.Range(-45, 44),Random.Range(-43,43) , 0); //x.y
+            Instantiate(food[Random.Range(0,food.Length)], randomSpwanPos, Quaternion.identity);
+            foodSpawn +=1;
+        }
         
     }
 }
